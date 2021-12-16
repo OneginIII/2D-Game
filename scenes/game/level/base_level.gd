ce6ca5425f554998c103bfa0b9300d4eb41c0638
Tero Salmela <tero.salmela@reun.eu>
2021-12-16 18:27:38 +0200

    Add tiles, base level features and water edge shader

---
+++ scenes/game/level/base_level.gd
@@ -0,0 +1,23 @@
+tool
+extends Node2D
+
+export(int) var level_length = 20 setget update_level_size
+export(float) var scroll_speed = 1.0
+
+var level_rect := Rect2()
+
+const TILE_SIZE := 128.0
+const LEVEL_WIDTH := 10
+
+func update_level_size(value):
+	level_length = value
+	level_rect = Rect2(Vector2.ZERO, Vector2(TILE_SIZE * LEVEL_WIDTH, TILE_SIZE * value))
+	level_rect.position.y -= level_rect.size.y - ProjectSettings.get_setting("display/window/size/height")
+	update()
+
+func _ready():
+	update_level_size(level_length)
+
+func _draw():
+	if Engine.editor_hint:
+		draw_rect(level_rect, Color.red, false, 10.0)
